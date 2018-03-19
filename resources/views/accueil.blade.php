bf6c1f4c865402ff26c3d79f4bfc40c3abbb12f4
Seb <allimant.sebastien@gmail.com>
2018-03-19 19:21:41 +0100

    page accueil sans les lien inscription, connection ni les sorties

---
+++ resources/views/accueil.blade.php
@@ -0,0 +1,53 @@
+@extends('template')
+
+@section('content')
+
+<body id="top">
+		<!-- Banner -->
+			<section id="banner">
+
+				<div class="inner">
+					<h2>Bienvenue sur OUT</h2>
+					<p>"Avec OUT <strong>sortez</strong> et <strong>profitez</strong> ensemble"</p>
+					<ul class="actions">
+						<li><a href="#content" class="button big special">Les Sorties</a></li>
+					</ul>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style1">
+				<header class="major">
+					<h2><p>Description site</p></h2>
+					<h2>Quelque example de sorties : </h2>
+				</header>
+				<div class="container">
+					<div class="row">
+						<div class="4u">
+							<section class="special box">
+								<i class="icon fa-bicycle major"></i>
+								<h3>Sortie velo</h3>
+								<p>hier nous avons fait une sortie vélo sur la journée, heureusement que la pause du déjeuner nous à permis de reprendre du poil de la bete pour repartir.</p>
+							</section>
+						</div>
+						<div class="4u">
+							<section class="special box">
+								<i class="icon fa-film major"></i>
+								<h3>Sortie cinéma</h3>
+								<p>Fin de semaine détente car nous sommes allé au cinema voir le nouveau film Marvel qui vient de sortir.</p>
+							</section>
+						</div>
+						<div class="4u">
+							<section class="special box">
+								<i class="icon fa-tint major"></i>
+								<h3>Sortie piscine</h3>
+								<p>Enfin, le soleil est de retour. Nous choisissons donc une petite sortit piscine pour les enfants et les adultes bien sûr.</p>
+							</section>
+						</div>
+					</div>
+				</div>
+			</section>	
+
+
+
+@endsection
